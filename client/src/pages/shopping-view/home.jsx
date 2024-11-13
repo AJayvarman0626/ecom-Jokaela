3af3432e1fac2e19553c55e0d2bcfd91cf7c8edd
AJayvarman <ajayvarma007j@gmail.com>
2024-11-13 18:39:44 +0530

    Update home.jsx

--- client/src/pages/shopping-view/home.jsx
+++ client/src/pages/shopping-view/home.jsx
@@ -188,7 +188,7 @@ function ShoppingHome() {
 
       <section className="py-12">
         <div className="container mx-auto px-4">
-          <h2 className="text-3xl font-bold text-center mb-8">Feature Products</h2>
+          <h2 className="text-3xl font-bold text-center mb-8">Rare & Unique Products</h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
             {productList && productList.length > 0
               ? productList.map((productItem) => (
